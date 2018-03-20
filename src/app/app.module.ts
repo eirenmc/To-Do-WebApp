963c80f8ca1405eca020ba3b9a09a9d8e57bb0be
Eiren McLoughlin <eiren@gomobile.ie>
2018-03-20 18:22:36 +0000

    Small Fixes, Responsiveness, Comments
    Implemented a few small changes such as fixing a small bug of input being empty yet creating a tasks once the initial to do task was created, but not works fine and does not allow for empty or null entries to create a to-do task.
    
    Added responsivness to the app so it visually looks fine no matter the size of the screen
    
    Added some comments in places to explain various elements in the code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Component Imports
 import { AppComponent } from './app.component';
 import { ToDoComponent } from './to-do/to-do.component';
-
 
 @NgModule({
   declarations: [
--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -22,48 +22,56 @@ import { trigger, style, transition, animate, keyframes, query, stagger } from '
 })
 
 export class ToDoComponent implements OnInit {
+  // Variables
   taskCount: number;
   notCompleted: number;
   todoText: string;
   taskStatus: boolean = false;
+
+  // Array to hold the to-dos, set up with 2 default entries to show off styling for completed and incomplete to-dos
   tasks = [
-    {taskText: 'Do the grocery shopping', complete: false},
-    {taskText: 'Pick up prescription', complete: true}
+    { taskText: 'Do the grocery shopping', complete: false },
+    { taskText: 'Pick up prescription', complete: true }
   ];
 
   constructor() { }
 
   ngOnInit() {
+    // On initilaizing of app, it sets the task counter for all the to-dos that was created
+    // Its used to visually display the amount of to-dos the user has created, both incomplete and complete
     this.taskCount = this.tasks.length;
-   /* for (let j = 0; j < this.tasks.length; j++) {
-     // console.log(this.tasks[j]);
-      if (this.tasks[j].complete === true) {
-        this.notCompleted = this.tasks[j].length;
-        console.log(this.notCompleted[j]);
-      }else{
-        this.notCompleted = 0;
-      }
-    }*/
   }
 
+  // Function to create a to-do task, that checks firstly that the value is not null or empty
   addToDoTask() {
     if (this.todoText != null) {
+      // Pushs the new to-do task with a complete value of false into the tasks array,
+      // as its a new entry and not something the user has completed yet
       this.tasks.push({taskText: this.todoText, complete: false});
-      this.todoText = '';
+
+      // resets todoText to null, thus emptying out the previous value that was entered
+      this.todoText = null;
+
+      // Updates the to-do counter based on the number of entries in the tasks array
       this.taskCount = this.tasks.length;
     }
   }
 
+  // Function to remove a specified to-do task from the list and update counter
   removeTask(i) {
     this.tasks.splice(i, 1);
     this.taskCount = this.tasks.length;
   }
 
+  // Function to remove all to-do tasks and update counter
   removeAllTasks() {
     this.tasks = [];
     this.taskCount = this.tasks.length;
   }
 
+  // Function to update a to-do tasks status, i.e complete or pending
+  // This function switches the value of whether its complete or pending 
+  // in the array, depending on which one is clicked on
   updateTaskStatus(i) {
     this.tasks[i].complete = !this.tasks[i].complete;
   }
